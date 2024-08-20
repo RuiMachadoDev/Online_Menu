cba6104809e67f138cfbfb6af2d83b4316a2ec0d
Rui Machado <ruifilipemachado@hotmail.com>
2024-08-20 16:58:08 +0100

    Change the vite file

--- Online_Menu/vite.config.js
+++ Online_Menu/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Diretório de saída para os arquivos de produção
+    outDir: 'dist',
   },
-  base: '',
+  base: './',
 })
